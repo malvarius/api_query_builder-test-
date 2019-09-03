33a0231d13f487d9827dec8f1006169cc49317fa
Eric Malvar <47836797+malvarius@users.noreply.github.com>
2019-09-03 11:09:18 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,106 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "api_builder"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Use Handlebars to render the main index.html page with the movies in it.
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM api_inputs;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { inputs: data });
+  });
+});
+
+// Create a new movie
+app.post("/api/string", function(req, res) {
+  connection.query("INSERT INTO api_builder (api_inputs) VALUES (?)", [req.body.movie], function(err, result) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new movie
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
+  });
+});
+
+// Retrieve all movies
+app.get("/api/string", function(req, res) {
+  connection.query("SELECT * FROM api_inputs;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.json(data);
+  });
+});
+
+// Update a movie
+app.put("/api/string/:id", function(req, res) {
+  connection.query("UPDATE api_inputs SET input1 = ? WHERE id = ?", [req.body.movie, req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Delete a movie
+app.delete("/api/movies/:id", function(req, res) {
+  connection.query("DELETE FROM apit_inputs WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    }
+    else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
